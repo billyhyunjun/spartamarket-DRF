da62857de4d3e24be753c18f9300a81a312973c6
Billyhyunjun <phjphj9999@gmail.com>
2024-04-30 20:02:27 +0900

    수정수정

--- products/views.py
+++ products/views.py
@@ -7,12 +7,22 @@ from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from .serializer import ProductSerializer, ProductDetailSerializer
 from accounts.models import User
+from django.db.models import Count
 
 
 class ProductsView(APIView):
     # 상점 목록 보기
     def get(self, request):
-        products = Product.objects.all()  # 전부 다 가져와
+        # 정렬 조건이 있다면 정렬해서 표기
+        sort = request.data.get("sort")
+        if sort:
+            if sort == "like_users":
+                products = Product.objects.annotate(num_likes=Count(sort)).order_by("-num_likes")
+            else:
+                return Response("Error: Wrong sort tag", status=status.HTTP_400_BAD_REQUEST)
+        # 정렬 조건이 없으면 모든 글 보여주기
+        else:
+            products = Product.objects.all()  # 전부 다 가져와
         serializer = ProductSerializer(products, many=True)  # 형식에 맞추어 데이터 생성
         return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -20,18 +30,23 @@ class ProductsView(APIView):
     # 로그인 상태에서만 가능, 상점 게시글 생성
     @permission_classes([IsAuthenticated])
     def post(self, request):
-        # 요청을 보낸 사용자의 정보를 사용하여 작성자 값을 설정합니다.
+        # 클라이언트에서 받아온 해시태그, 카테고리, 작성자아이디.
         categories = request.data.get("categories")
         hashtags = request.data.get("hashtags")
         request.data["author"] = request.user.id
         serializer = ProductSerializer(data=request.data)
+        # 유효성 검사
         if serializer.is_valid(raise_exception=True):
+            # 일단 해시태그가 없는 값으로 먼저 저장
             serializer.save()
+            # 방금 저장했던 게시글 가져오기
             product = Product.objects.all().last()
+            # 해시태그 하나씩 꺼내서 중복 검사하고 게시글에 저장
             for hashtag_data in hashtags:
+                hashtag_data['tag'] = hashtag_data['tag'].upper()
                 hashtag, _ = Hashtag.objects.get_or_create(tag=hashtag_data['tag'])
                 product.hashtags.add(hashtag)
-            
+            # 카테고리도 마찬가지로 중복검사 및 저장
             for category_data in categories:
                 category, _ = Category.objects.get_or_create(name=category_data['name'])
                 product.categories.add(category)
